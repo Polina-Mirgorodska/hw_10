cffad0d78e8573a7824220b5e03df1280f8b3ca4
Polina-Mirgorodska <mirgorodskapolina@gmail.com>
2026-04-08 17:57:07 +0200

    111

---
+++ index.js
@@ -0,0 +1,78 @@
+const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
+const words = ['кіт', 'собака', 'миша', 'папуга', 'хомяк'];
+
+// Функція countItems тут
+function countItems(array, condition) {
+    let count = 0;
+    for (const element of array){
+        if(condition(element)){
+            count += 1;
+        }
+    }  
+    return count  
+}
+
+// Умови для перевірки:
+const isEven = num => num % 2 === 0;
+const isLarge = num => num > 10;
+const isShort = word => word.length <= 3;
+
+// Тестування
+console.log('Парних чисел:', countItems(numbers, isEven));
+console.log('Чисел більше 10:', countItems(numbers, isLarge));
+console.log('Коротких слів:', countItems(words, isShort));
+
+
+
+
+// Функція calculate тут
+const calculate = (a, b, operation) => {
+    return operation(a,b)
+}
+// Стрілкові функції для операцій тут
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => {
+    if (b === 0){
+        return "Помилка ділення на нуль"
+    }
+    return a/b
+}
+// Тестування
+console.log(calculate(10, 5, add));      // Повинно показати 15
+console.log(calculate(10, 5, subtract)); // Повинно показати 5
+console.log(calculate(10, 5, multiply)); // Повинно показати 50
+console.log(calculate(10, 5, divide));   // Повинно показати 2
+console.log(calculate(10, 0, divide));   // Повинно показати помилку
+
+
+
+
+function repeatMessage(times, messageCreator) {
+  for (let i = 0; i < times; i+=1) {
+    console.log(messageCreator(i));
+  }
+}
+
+const helloMessage = index => `Привіт! №${index + 1}`;
+
+repeatMessage(5, helloMessage);
+
+
+
+
+
+const movies = [
+  { title: "Inception", year: 2010, rating: 8.8 },
+  { title: "Titanic", year: 1997, rating: 7.9 },
+  { title: "Interstellar", year: 2014, rating: 8.6 }
+];
+
+function processMovies(movies, action) {
+    let index = 0
+    for (const movie of movies){
+        action(movie, index);
+        index += 1;
+    }
+}
